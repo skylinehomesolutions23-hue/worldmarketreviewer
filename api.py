b29313db55e868fb7b638e3d0dbb8718d119a741
Greg Herman <skylinehomesolutions23@gmail.com>
2026-01-10 23:47:46 -0500

    Update API to use data folder paths

--- api.py
+++ api.py
@@ -1,9 +1,12 @@
-
 from fastapi import FastAPI
 from build_mobile_summary import main
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/api/summary")
 def get_summary():
     return main()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
